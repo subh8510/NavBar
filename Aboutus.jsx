dd94b32fa411979650ec9ab3bf8663a43e85d28e
subh8510 <152373689+subh8510@users.noreply.github.com>
2024-10-24 22:48:27 +0530

    Update Aboutus.jsx

--- Aboutus.jsx
+++ Aboutus.jsx
@@ -5,7 +5,7 @@ const Aboutus = () => {
     <div>
            <div className='flex justify-center items-center h-screen p-20 w-full bg-teal-200'>
       <div className='container flex-col gap-20 h-[80%] bg-amber-600 font-extrabold text-2xl rounded-2xl text-white flex justify-center items-center'>
-          <h2 className='w-1/2 text-center'>Mera naam Sachin hai aur aaj hum React kr rhe hain humne abhi abhi <span className='text-3xl text-yellow-300 font-bold'>react-router-dom</span> sikha hai aur ye uska </h2>
+          <h2 className='w-1/2 text-center'>Mera naam Alok hai aur aaj hum React kr rhe hain humne abhi abhi <span className='text-3xl text-yellow-300 font-bold'>react-router-dom</span> sikha hai aur ye uska </h2>
           <h1>mera roll no. 233071</h1>
       </div>
     </div>
